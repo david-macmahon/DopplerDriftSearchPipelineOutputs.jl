82b31070768403b8acbb89fc779df7dd0474da02
David MacMahon <davidm@astro.berkeley.edu>
2024-01-09 23:03:16 -0800

    Add nchans, nsamps, and nrates metadata fields
    Old `numrates` name for `nrates` is still supported.  It is silently
    translated to `nrates` in DataFrame (input file remains unchanged).

--- src/loadhits.jl
+++ src/loadhits.jl
@@ -14,11 +14,15 @@ function loadhits(arrowname::AbstractString)::DataFrame
     for (k,v) in Arrow.getmetadata(tbl)
         if k in ("datafile", "source_name")
             vany = v
-        elseif k in ("nfpc", "numrates")
+        elseif k in ("nfpc", "nchans", "nsamps", "nrates")
             vany = something(tryparse(Int, v), v)
         elseif k in ("startrate", "deltarate", "snr", "radius",
                      "fch1", "foff", "tstart", "tsamp", "ra", "dec", "dfdt")
             vany = something(tryparse(Float64, v), v)
+        # `numrates` was original name of `nrates`
+        elseif k == "numrates"
+            k = "nrates" # Silently translate to new name
+            vany = something(tryparse(Int, v), v)
         else
             @warn "unknown metadata field $k"
             vany = v
